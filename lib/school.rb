33707afa18e4df326b93a1abfaf3302819ccc8b3
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-16 18:16:23 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+
+  attr_accessor :roster
+
+  def add_student(student_name, grade)
+      roster[grade] ||= []
+      roster[grade] << student_name
+
+    def add_student(name, grade)
+      @roster << name[grade]
+    end
+
+end
